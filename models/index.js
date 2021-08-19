5b1d2cc26ffcf9008c5fea7c164fc6e6a4982d32
Daniel Carazo <danielcarazo20@gmail.com>
2021-08-19 18:46:08 -0400

    finalized all models and association in models/index file

--- models/index.js
+++ models/index.js
@@ -13,12 +13,14 @@ Category.hasMany(Product, {
   foreignKey: 'category_id'
 })
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+})
 
-module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
+module.exports = { Product, Category, Tag, ProductTag };
